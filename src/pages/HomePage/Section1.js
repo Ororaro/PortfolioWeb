5a2e21db6a06c807701dbcad04d1a8e8e6e3a759
Ororaro <framelenny@gmail.com>
2023-04-30 14:51:14 +0700

    edit text section 1 homepage

--- src/pages/HomePage/Section1.js
+++ src/pages/HomePage/Section1.js
@@ -14,7 +14,7 @@ const Section1 = () => {
             <Typography variant='h3' sx={{fontSize:{xs:'50px !important'}}}>Hi! </Typography>
             <Typography variant='h4' sx={{fontSize:{xs:'50px !important'}}}>I'm <span style={{color: '#9b23ea',fontWeight: '700',letterSpacing:'1px' }}>Frame</span> </Typography>
             <Typography variant='h4' sx={{fontWeight: '700',letterSpacing:'1px',fontSize:{xs:'50px !important'} }}>Frontend Developer</Typography>
-            <Typography variant='h5' sx={{fontWeight: '700' ,letterSpacing:'1px'}}>My name is Natpakal Chuejad<span style={{color: '#9b23ea',fontWeight: '700',letterSpacing:'1px' }}> (Frame) </span> from Thailand I enjoy creating website and  learning something new </Typography>
+            <Typography variant='h5' sx={{fontWeight: '700' ,letterSpacing:'1px'}}>My name is Natpakal Chuejad<span style={{color: '#9b23ea',fontWeight: '700',letterSpacing:'1px' }}> (Frame) </span> I enjoy creating website and  learning something new </Typography>
             <Box className="control-btn">
               <Button onClick={handleClickNav}>Contact Me</Button>
             </Box>
